6bebe7ca1eb23596d163fb9e939da6446f638c8e
구병국 <@DESKTOP-G661P2L>
2017-10-22 12:32:54 +0900

    12시 32분

--- src/exercise02/Caculator.java
+++ src/exercise02/Caculator.java
@@ -13,15 +13,21 @@ public class Caculator {
 
 	/**
 	 * @param args
-	 * 사용자가 두개의 값을 입력하면 두수를 이용하여 사칙연산을 만들어 보시오.(변수사용)
+	 * 사용자가 두개의 값과 연산기호를 입력받아 연산을 만들어 보시오.(변수,if문사용)
+	 * 문자s를  숫자로 바꿔주는 메서드는 Integer.parseInt(s)
+	 * equals메서드 사용(A와 B글자가 같은 내용인지 알아볼때 A.equals(B) 형태로 사용 결과는 true,false로 전달함
+	 * 
 	 */
 //	출력형태
 //	첫번째 수를 입력하세요.4
 //	두번째 수를 입력하세요.3
-//	덧셈 : 4+3 = 7
-//	뺄샘 : 4-3 = 1
+//	연산기호를 입력하시오(+,-,*,/). *
 //	곱셈 : 4*3 = 12
-//	나눗셈 : 4/3 = 1
+//	
+//	
+//	만일 연산기호가 아니면 아래와 같은 출력형태를 나타낸다.
+//	연산기호를 알수 없습니다.
+	
 	
 	public static void main(String[] args) {
 
@@ -32,9 +38,9 @@ public class Caculator {
 	/**
 	 * 
 	 */
-	private static int getUserInput() {
+	private static String getUserInput() {
 		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
+		String input = sc.next();
 		return input;
 		
 	}
