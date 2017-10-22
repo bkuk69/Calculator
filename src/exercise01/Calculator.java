a159970992b2afd0ef749deb8effdb31bed947b9
구병국 <@DESKTOP-G661P2L>
2017-10-22 11:49:43 +0900

    사칙연산

---
+++ src/exercise01/Calculator.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package exercise01;
+
+/**
+ * @since 2017. 10. 22.
+ * @author ±¸º´±¹
+ */
+public class Calculator {
+
+	/**
+	 * @param args
+	 * »çÄ¢¿¬»ê ¸¸µé¾î º¸±â¸¦ ÇØº¸¼¼¿ä. 4,3
+	 */
+	public static void main(String[] args) {
+		System.out.println("µ¡¼À : " +(3+4));
+		System.out.println("»¬¼À : " +(4-3));
+		System.out.println("°ö¼À : " +(4*3));
+		System.out.println("³ª´°¼À : " +(4/3));
+
+	}
+
+}
