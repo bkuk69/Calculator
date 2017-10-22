6a545ca041ce0ac76255d59f780155c0c6f978b5
구병국 <@DESKTOP-G661P2L>
2017-10-22 19:57:03 +0900

    클래스 형태로 만든 메서드

---
+++ src/exercise06/Input.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package exercise06;
+
+/**
+ * @since 2017. 10. 22.
+ * @author 구병국
+ * 숫자와 문자를 입력받는 메서드들을 정한다.getIntegerInput, getStringInput
+ * 메서드의 내용 구현은 앞의 연습에 있는 것들을 참조한다.
+ */
+public class Input {
+
+}
---
+++ src/exercise06/Output.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package exercise06;
+
+/**
+ * @since 2017. 10. 22.
+ * @author 구병국
+ * CaculatorDrive 클래스에 나타난 내용에 따라 printInfo메서드들을 만든다.
+ */
+public class Output {
+
+}
