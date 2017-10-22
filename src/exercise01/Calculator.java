e8d2e3941a0b1300a9ab45c0c1f94e5d28d7b119
구병국 <@DESKTOP-G661P2L>
2017-10-22 11:50:06 +0900

    10시 22분

--- src/exercise01/Calculator.java
+++ src/exercise01/Calculator.java
@@ -13,11 +13,13 @@ public class Calculator {
 	 * @param args
 	 * »çÄ¢¿¬»ê ¸¸µé¾î º¸±â¸¦ ÇØº¸¼¼¿ä. 4,3
 	 */
+//	Ãâ·ÂÇüÅÂ
+//	µ¡¼À : 7
+//	»¬¼À : 1
+//	°ö¼À : 12
+//	³ª´°¼À : 1
 	public static void main(String[] args) {
-		System.out.println("µ¡¼À : " +(3+4));
-		System.out.println("»¬¼À : " +(4-3));
-		System.out.println("°ö¼À : " +(4*3));
-		System.out.println("³ª´°¼À : " +(4/3));
+		
 
 	}
 
